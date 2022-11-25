8931f89829b8bd381a75d1ecb6acfd537dd6bd78
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-11-25 09:02:21 +0300

    add vault token account

--- tests/ft.ts
+++ tests/ft.ts
@@ -19,6 +19,8 @@ describe("ft", () => {
 
   let user_sending = provider.wallet.publicKey;
 
+  let vault_account = anchor.web3.Keypair.generate();
+
   let user_receiving = anchor.web3.Keypair.generate();
 
   let vault: anchor.web3.PublicKey;
@@ -34,14 +36,21 @@ describe("ft", () => {
     spl.TOKEN_PROGRAM_ID,
   );
 
-  it("Initializes Fund Transfer", async () => {
-    //airdrop tokens to payer
-    await provider.connection.requestAirdrop(
-      payer.publicKey, 1 * LAMPORTS_PER_SOL
-    )
-  });
+      //sender's token account
+    let sender_token_account = await mint.createAccount(user_sending);
+    await mint.mintTo(sender_token_account, payer, [], 10_000_000);//if we can be rich, why not?
 
-  })
+    //vault token account
+    let vault_token_account = await mint.createAccount(vault_account.publicKey)
+
+    it("Initializes Fund Transfer", async () => {
+      //airdrop tokens to payer
+      await provider.connection.requestAirdrop(
+        payer.publicKey, 1 * LAMPORTS_PER_SOL
+      )
+    });
+
+  });
 
   
 });
