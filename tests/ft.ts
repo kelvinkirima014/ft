67a2a8d2408433809c1001191fc91d52e4d764c1
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-11-25 08:39:22 +0300

    ...and we've a test liftoff

--- tests/ft.ts
+++ tests/ft.ts
@@ -4,6 +4,7 @@ import * as spl from '@solana/spl-token';
 import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
 import { LAMPORTS_PER_SOL, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
 import { Ft } from "../target/types/ft";
+import { SplTokenAccountsCoder } from "@project-serum/anchor/dist/cjs/coder/spl-token/accounts";
 
 describe("ft", () => {
   // Configure the client to use the local cluster.
@@ -22,10 +23,25 @@ describe("ft", () => {
 
   let vault: anchor.web3.PublicKey;
 
+  before(async() => {
+
+    let mint = await spl.Token.createMint(
+    provider.connection,
+    payer,
+    provider.wallet.publicKey,
+    provider.wallet.publicKey,
+    7,
+    spl.TOKEN_PROGRAM_ID,
+  );
+
   it("Initializes Fund Transfer", async () => {
     //airdrop tokens to payer
     await provider.connection.requestAirdrop(
       payer.publicKey, 1 * LAMPORTS_PER_SOL
     )
   });
+
+  })
+
+  
 });
