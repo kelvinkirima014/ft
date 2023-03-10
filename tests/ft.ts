a8885e33a4c5c54e3b756131f8e1a2f8371a2050
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-03-10 23:26:06 +0300

    init a transfer

--- tests/ft.ts
+++ tests/ft.ts
@@ -7,5 +7,22 @@ import { Ft } from "../target/types/ft";
 import { SplTokenAccountsCoder } from "@project-serum/anchor/dist/cjs/coder/spl-token/accounts";
 
 describe("ft", () => {
- 
+
+  //configure the client to use the desired cluster
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  //main wallet to fund transactions
+  const payer = provider.wallet as anchor.Wallet;
+
+  //our Rust onchain program IDL
+  const program = anchor.workspace.ft as Program<Ft>;
+
+  //keypair that represents our token
+  const mintKeypair = anchor.web3.Keypair.generate();
+
+  it("initializes transfer", async() => {
+    
+  })
+
 });
